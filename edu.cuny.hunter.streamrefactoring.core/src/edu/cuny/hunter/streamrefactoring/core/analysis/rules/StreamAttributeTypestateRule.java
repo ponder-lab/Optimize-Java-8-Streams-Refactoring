4c78beb7e8e6b80581f0762a10ad18e9f3fc0a0e
Raffi Khatchadourian <raffi.khatchadourian@hunter.cuny.edu>
2017-03-14 16:22:45 -0400

    Extract a superclass for #10.

---
+++ edu.cuny.hunter.streamrefactoring.core/src/edu/cuny/hunter/streamrefactoring/core/analysis/rules/StreamAttributeTypestateRule.java
@@ -0,0 +1,60 @@
+package edu.cuny.hunter.streamrefactoring.core.analysis.rules;
+
+import com.ibm.safe.dfa.DFASpec;
+import com.ibm.safe.dfa.DFAState;
+import com.ibm.safe.dfa.DFATransition;
+import com.ibm.safe.dfa.IDFAState;
+import com.ibm.safe.dfa.IDFATransition;
+import com.ibm.safe.dfa.events.IDispatchEvent;
+import com.ibm.safe.dfa.events.IDispatchEventImpl;
+import com.ibm.safe.rules.TypestateRule;
+import com.ibm.wala.classLoader.IClass;
+
+public abstract class StreamAttributeTypestateRule extends TypestateRule {
+
+	public StreamAttributeTypestateRule(IClass streamClass) {
+		this.addType(streamClass.getName().toString());
+		this.setTypeStateAutomaton(new DFASpec());
+		this.addAutomaton();
+	}
+
+	protected abstract void addAutomaton();
+
+	protected IDFAState addState(String stateName, boolean initialState) {
+		IDFAState state = new DFAState();
+
+		state.setName(stateName);
+		this.getTypeStateAutomaton().addState(state);
+
+		if (initialState)
+			this.getTypeStateAutomaton().setInitialState(state);
+
+		return state;
+	}
+
+	protected IDFATransition addTransition(IDFAState source, IDFAState destination, IDispatchEvent event) {
+		IDFATransition transition = new DFATransition();
+	
+		transition.setSource(source);
+		transition.setEvent(event);
+		transition.setDestination(destination);
+	
+		this.getTypeStateAutomaton().addTransition(transition);
+		return transition;
+	}
+
+	protected IDispatchEvent addEvent(String eventName, String eventPattern) {
+		IDispatchEventImpl event = new IDispatchEventImpl();
+	
+		event.setName(eventName);
+		event.setPattern(eventPattern);
+	
+		this.getTypeStateAutomaton().addEvent(event);
+		return event;
+	}
+
+	protected IDFAState addState(String stateName) {
+		return addState(stateName, false);
+	}
+
+}
--- edu.cuny.hunter.streamrefactoring.core/src/edu/cuny/hunter/streamrefactoring/core/analysis/rules/StreamExecutionModeTypeStateRule.java
+++ edu.cuny.hunter.streamrefactoring.core/src/edu/cuny/hunter/streamrefactoring/core/analysis/rules/StreamExecutionModeTypeStateRule.java
@@ -1,127 +1,81 @@
 package edu.cuny.hunter.streamrefactoring.core.analysis.rules;
 
-import com.ibm.safe.dfa.DFASpec;
-import com.ibm.safe.dfa.DFAState;
-import com.ibm.safe.dfa.DFATransition;
 import com.ibm.safe.dfa.IDFAState;
-import com.ibm.safe.dfa.IDFATransition;
 import com.ibm.safe.dfa.events.IDispatchEvent;
-import com.ibm.safe.dfa.events.IDispatchEventImpl;
-import com.ibm.safe.rules.TypestateRule;
 import com.ibm.wala.classLoader.IClass;
 
-public class StreamExecutionModeTypeStateRule extends TypestateRule {
+public class StreamExecutionModeTypeStateRule extends StreamAttributeTypestateRule {
 
 	public StreamExecutionModeTypeStateRule(IClass streamClass) {
-		super();
-		this.addType(streamClass.getName().toString());
+		super(streamClass);
 		this.setName("execution mode");
-		this.addAutomaton();
 	}
 
-	private void addAutomaton() {
-		DFASpec automaton = new DFASpec();
-
+	@Override
+	protected void addAutomaton() {
 		// a bottom state result would need to defer to the initial stream
 		// ordering, which is in the field of the stream.
-		IDFAState bottomBottomState = addState(automaton, "bottomBottom", true);
+		IDFAState bottomBottomState = addState("bottomBottom", true);
+				
+		IDFAState sequentialBottomState = addState("sequentialBottom");
+		IDFAState parallelBottomState = addState("parallelBottom");
+		IDFAState bottomOrderedState = addState("bottomOrdered");
+		IDFAState bottomUnorderedState = addState("bottomUnordered");
 
-		IDFAState sequentialBottomState = addState(automaton, "sequentialBottom");
-		IDFAState parallelBottomState = addState(automaton, "parallelBottom");
-		IDFAState bottomOrderedState = addState(automaton, "bottomOrdered");
-		IDFAState bottomUnorderedState = addState(automaton, "bottomUnordered");
+		IDFAState sequentialOrderedState = addState("sequentialOrdered");
+		IDFAState sequentialUnorderedState = addState("sequentialUnordered");
+		IDFAState parallelOrderedState = addState("parallelOrdered");
+		IDFAState parallelUnorderedState = addState("parallelUnordered");
 
-		IDFAState sequentialOrderedState = addState(automaton, "sequentialOrdered");
-		IDFAState sequentialUnorderedState = addState(automaton, "sequentialUnordered");
-		IDFAState parallelOrderedState = addState(automaton, "parallelOrdered");
-		IDFAState parallelUnorderedState = addState(automaton, "parallelUnordered");
-
-		IDispatchEvent parallelEvent = addEvent(automaton, "parallel", ".*parallel\\(\\).*");
-		IDispatchEvent sequentialEvent = addEvent(automaton, "sequential", ".*sequential\\(\\).*");
-		IDispatchEvent sortedEvent = addEvent(automaton, "sorted", ".*sorted\\(\\).*");
-		IDispatchEvent unorderedEvent = addEvent(automaton, "unordered", ".*unordered\\(\\).*");
+		IDispatchEvent parallelEvent = addEvent("parallel", ".*parallel\\(\\).*");
+		IDispatchEvent sequentialEvent = addEvent("sequential", ".*sequential\\(\\).*");
+		IDispatchEvent sortedEvent = addEvent("sorted", ".*sorted\\(\\).*");
+		IDispatchEvent unorderedEvent = addEvent("unordered", ".*unordered\\(\\).*");
 
 		// TODO: Need to add concat().
-		addTransition(automaton, bottomBottomState, parallelBottomState, parallelEvent);
-		addTransition(automaton, bottomBottomState, sequentialBottomState, sequentialEvent);
-		addTransition(automaton, bottomBottomState, bottomOrderedState, sortedEvent);
-		addTransition(automaton, bottomBottomState, bottomUnorderedState, unorderedEvent);
+		addTransition(bottomBottomState, parallelBottomState, parallelEvent);
+		addTransition(bottomBottomState, sequentialBottomState, sequentialEvent);
+		addTransition(bottomBottomState, bottomOrderedState, sortedEvent);
+		addTransition(bottomBottomState, bottomUnorderedState, unorderedEvent);
 
-		addTransition(automaton, sequentialBottomState, parallelBottomState, parallelEvent);
-		addTransition(automaton, sequentialBottomState, sequentialBottomState, sequentialEvent);
-		addTransition(automaton, sequentialBottomState, sequentialOrderedState, sortedEvent);
-		addTransition(automaton, sequentialBottomState, sequentialUnorderedState, unorderedEvent);
+		addTransition(sequentialBottomState, parallelBottomState, parallelEvent);
+		addTransition(sequentialBottomState, sequentialBottomState, sequentialEvent);
+		addTransition(sequentialBottomState, sequentialOrderedState, sortedEvent);
+		addTransition(sequentialBottomState, sequentialUnorderedState, unorderedEvent);
 
-		addTransition(automaton, parallelBottomState, parallelBottomState, parallelEvent);
-		addTransition(automaton, parallelBottomState, sequentialBottomState, sequentialEvent);
-		addTransition(automaton, parallelBottomState, parallelOrderedState, sortedEvent);
-		addTransition(automaton, parallelBottomState, parallelUnorderedState, unorderedEvent);
+		addTransition(parallelBottomState, parallelBottomState, parallelEvent);
+		addTransition(parallelBottomState, sequentialBottomState, sequentialEvent);
+		addTransition(parallelBottomState, parallelOrderedState, sortedEvent);
+		addTransition(parallelBottomState, parallelUnorderedState, unorderedEvent);
 
-		addTransition(automaton, bottomUnorderedState, parallelUnorderedState, parallelEvent);
-		addTransition(automaton, bottomUnorderedState, sequentialUnorderedState, sequentialEvent);
-		addTransition(automaton, bottomUnorderedState, bottomOrderedState, sortedEvent);
-		addTransition(automaton, bottomUnorderedState, bottomUnorderedState, unorderedEvent);
+		addTransition(bottomUnorderedState, parallelUnorderedState, parallelEvent);
+		addTransition(bottomUnorderedState, sequentialUnorderedState, sequentialEvent);
+		addTransition(bottomUnorderedState, bottomOrderedState, sortedEvent);
+		addTransition(bottomUnorderedState, bottomUnorderedState, unorderedEvent);
 
-		addTransition(automaton, bottomOrderedState, parallelOrderedState, parallelEvent);
-		addTransition(automaton, bottomOrderedState, sequentialOrderedState, sequentialEvent);
-		addTransition(automaton, bottomOrderedState, bottomOrderedState, sortedEvent);
-		addTransition(automaton, bottomOrderedState, bottomUnorderedState, unorderedEvent);
+		addTransition(bottomOrderedState, parallelOrderedState, parallelEvent);
+		addTransition(bottomOrderedState, sequentialOrderedState, sequentialEvent);
+		addTransition(bottomOrderedState, bottomOrderedState, sortedEvent);
+		addTransition(bottomOrderedState, bottomUnorderedState, unorderedEvent);
 
-		addTransition(automaton, sequentialUnorderedState, parallelUnorderedState, parallelEvent);
-		addTransition(automaton, sequentialUnorderedState, sequentialUnorderedState, sequentialEvent);
-		addTransition(automaton, sequentialUnorderedState, sequentialOrderedState, sortedEvent);
-		addTransition(automaton, sequentialUnorderedState, sequentialUnorderedState, unorderedEvent);
+		addTransition(sequentialUnorderedState, parallelUnorderedState, parallelEvent);
+		addTransition(sequentialUnorderedState, sequentialUnorderedState, sequentialEvent);
+		addTransition(sequentialUnorderedState, sequentialOrderedState, sortedEvent);
+		addTransition(sequentialUnorderedState, sequentialUnorderedState, unorderedEvent);
 
-		addTransition(automaton, sequentialOrderedState, parallelOrderedState, parallelEvent);
-		addTransition(automaton, sequentialOrderedState, sequentialOrderedState, sequentialEvent);
-		addTransition(automaton, sequentialOrderedState, sequentialOrderedState, sortedEvent);
-		addTransition(automaton, sequentialOrderedState, sequentialUnorderedState, unorderedEvent);
+		addTransition(sequentialOrderedState, parallelOrderedState, parallelEvent);
+		addTransition(sequentialOrderedState, sequentialOrderedState, sequentialEvent);
+		addTransition(sequentialOrderedState, sequentialOrderedState, sortedEvent);
+		addTransition(sequentialOrderedState, sequentialUnorderedState, unorderedEvent);
 
-		addTransition(automaton, parallelUnorderedState, parallelUnorderedState, parallelEvent);
-		addTransition(automaton, parallelUnorderedState, sequentialUnorderedState, sequentialEvent);
-		addTransition(automaton, parallelUnorderedState, parallelOrderedState, sortedEvent);
-		addTransition(automaton, parallelUnorderedState, parallelUnorderedState, unorderedEvent);
+		addTransition(parallelUnorderedState, parallelUnorderedState, parallelEvent);
+		addTransition(parallelUnorderedState, sequentialUnorderedState, sequentialEvent);
+		addTransition(parallelUnorderedState, parallelOrderedState, sortedEvent);
+		addTransition(parallelUnorderedState, parallelUnorderedState, unorderedEvent);
 
-		addTransition(automaton, parallelOrderedState, parallelOrderedState, parallelEvent);
-		addTransition(automaton, parallelOrderedState, sequentialOrderedState, sequentialEvent);
-		addTransition(automaton, parallelOrderedState, parallelOrderedState, sortedEvent);
-		addTransition(automaton, parallelOrderedState, parallelUnorderedState, unorderedEvent);
-
-		this.setTypeStateAutomaton(automaton);
+		addTransition(parallelOrderedState, parallelOrderedState, parallelEvent);
+		addTransition(parallelOrderedState, sequentialOrderedState, sequentialEvent);
+		addTransition(parallelOrderedState, parallelOrderedState, sortedEvent);
+		addTransition(parallelOrderedState, parallelUnorderedState, unorderedEvent);
 	}
-
-	private static IDFATransition addTransition(DFASpec automaton, IDFAState source, IDFAState destination,
-			IDispatchEvent event) {
-		IDFATransition transition = new DFATransition();
-		transition.setSource(source);
-		transition.setEvent(event);
-		transition.setDestination(destination);
-		automaton.addTransition(transition);
-		return transition;
-	}
-
-	private static IDispatchEvent addEvent(DFASpec automaton, String eventName, String eventPattern) {
-		IDispatchEventImpl close = new IDispatchEventImpl();
-		close.setName(eventName);
-		close.setPattern(eventPattern);
-		automaton.addEvent(close);
-		return close;
-	}
-
-	private static IDFAState addState(DFASpec automaton, String stateName) {
-		return addState(automaton, stateName, false);
-	}
-
-	private static IDFAState addState(DFASpec automaton, String stateName, boolean initialState) {
-		IDFAState state = new DFAState();
-
-		state.setName(stateName);
-		automaton.addState(state);
-
-		if (initialState)
-			automaton.setInitialState(state);
-
-		return state;
-	}
-
 }
