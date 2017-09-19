02ab00dce461da16f70309ecc1d018bdf848e4d8
Yiming Tang <tangyiming0717@outlook.com>
2017-09-19 16:28:20 -0400

    fix a warning

--- edu.cuny.hunter.streamrefactoring.core/src/edu/cuny/hunter/streamrefactoring/core/analysis/RequireTerminalOperationException.java
+++ edu.cuny.hunter.streamrefactoring.core/src/edu/cuny/hunter/streamrefactoring/core/analysis/RequireTerminalOperationException.java
@@ -2,6 +2,11 @@ package edu.cuny.hunter.streamrefactoring.core.analysis;
 
 public class RequireTerminalOperationException extends Exception{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public RequireTerminalOperationException(String message) {
 	    		super(message);
 	}
