51def7a03e64c883a60a934cded4eae0cf8f84c4
Yiming Tang <tangyiming0717@outlook.com>
2017-09-20 16:19:05 -0400

    change format

--- edu.cuny.hunter.streamrefactoring.core/src/edu/cuny/hunter/streamrefactoring/core/analysis/RequireTerminalOperationException.java
+++ edu.cuny.hunter.streamrefactoring.core/src/edu/cuny/hunter/streamrefactoring/core/analysis/RequireTerminalOperationException.java
@@ -1,14 +1,11 @@
 package edu.cuny.hunter.streamrefactoring.core.analysis;
 
-public class RequireTerminalOperationException extends Exception{
+public class RequireTerminalOperationException extends Exception {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
 	public RequireTerminalOperationException(String message) {
-	    		super(message);
+		super(message);
 	}
 
 }
