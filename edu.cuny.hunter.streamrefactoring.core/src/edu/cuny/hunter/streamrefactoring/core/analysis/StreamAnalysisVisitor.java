5e7376e85cbf2e977c2f7d71040912fa5f145aea
Yiming Tang <tangyiming0717@outlook.com>
2017-09-21 17:37:53 -0400

    #47 delete the needless exception

--- edu.cuny.hunter.streamrefactoring.core/src/edu/cuny/hunter/streamrefactoring/core/analysis/StreamAnalysisVisitor.java
+++ edu.cuny.hunter.streamrefactoring.core/src/edu/cuny/hunter/streamrefactoring/core/analysis/StreamAnalysisVisitor.java
@@ -50,9 +50,7 @@ public class StreamAnalysisVisitor extends ASTVisitor {
 			} catch (ClassHierarchyException | IOException | CoreException | InvalidClassFileException | CancelException e) {
 				logger.log(Level.SEVERE, "Encountered exception while processing: " + node, e);
 				throw new RuntimeException(e);
-			} catch (RequireTerminalOperationException e) {
-				logger.log(Level.WARNING, "Require Terminal Operations!", e);
-			}
+			} 
 			this.getStreamSet().add(stream);
 			
 			stream.check();
