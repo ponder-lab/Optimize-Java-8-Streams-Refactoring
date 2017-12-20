1a4d1a4c71293ac707c9cbe8bd30150abc16cc29
Raffi Khatchadourian <raffi.khatchadourian@hunter.cuny.edu>
2017-12-20 14:03:46 -0500

    Shorten test for #119.
    Let's try to test only essential functionality.

--- edu.cuny.hunter.streamrefactoring.tests/resources/ConvertStreamToParallel/testWithoutEntryPoint/in/A.java
+++ edu.cuny.hunter.streamrefactoring.tests/resources/ConvertStreamToParallel/testWithoutEntryPoint/in/A.java
@@ -3,14 +3,8 @@ package p;
 import java.util.HashSet;
 
 class A {
-
 	void m() {
 		HashSet h1 = new HashSet();
 		h1.stream().count();
-	}
-
-	void n() {
-		HashSet h2 = new HashSet();
-		h2.stream().count();
 	}
 }
