5f8c769307c87a1b870fac08c9e3be57597ce9aa
Raffi Khatchadourian <raffi.khatchadourian@hunter.cuny.edu>
2018-01-06 09:53:24 -0500

    Add ctor test.

--- edu.cuny.hunter.streamrefactoring.tests/test cases/edu/cuny/hunter/streamrefactoring/ui/tests/ConvertStreamToParallelRefactoringTest.java
+++ edu.cuny.hunter.streamrefactoring.tests/test cases/edu/cuny/hunter/streamrefactoring/ui/tests/ConvertStreamToParallelRefactoringTest.java
@@ -364,6 +364,13 @@ public class ConvertStreamToParallelRefactoringTest extends RefactoringTest {
 				Refactoring.CONVERT_SEQUENTIAL_STREAM_TO_PARALLEL, RefactoringStatus.OK, Collections.emptySet()));
 	}
 
+	public void testConstructor() throws Exception {
+		helper(new StreamAnalysisExpectedResult("new ArrayList().stream()",
+				Collections.singleton(ExecutionMode.SEQUENTIAL), EnumSet.of(Ordering.ORDERED), false, false, false,
+				EnumSet.of(TransformationAction.CONVERT_TO_PARALLEL), PreconditionSuccess.P2,
+				Refactoring.CONVERT_SEQUENTIAL_STREAM_TO_PARALLEL, RefactoringStatus.OK, Collections.emptySet()));
+	}
+
 	public void testBitSet() throws Exception {
 		helper(new StreamAnalysisExpectedResult("set.stream()", Collections.singleton(ExecutionMode.SEQUENTIAL),
 				Collections.singleton(Ordering.ORDERED), false, false, false,
