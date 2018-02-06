23549f6b83a52acc8933c5b77c7d2e19454aa4dd
Raffi Khatchadourian <raffi.khatchadourian@hunter.cuny.edu>
2018-02-06 15:20:12 -0500

    Trying to fix testConcurrentReduction() for #64.

--- edu.cuny.hunter.streamrefactoring.tests/resources/ConvertStreamToParallel/testConcurrentReduction/in/A.java
+++ edu.cuny.hunter.streamrefactoring.tests/resources/ConvertStreamToParallel/testConcurrentReduction/in/A.java
@@ -2,18 +2,30 @@ package p;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ConcurrentNavigableMap;
+import java.util.concurrent.ConcurrentSkipListMap;
 import java.util.stream.Collectors;
+import java.util.Set;
 
-import edu.cuny.hunter.streamrefactoring.annotations.*;
+import edu.cuny.hunter.streamrefactoring.annotations.EntryPoint;
+
 import p.A.Widget.Color;
 
 public class A {
 
 	static class Widget {
 		enum Color {
-			RED, BLUE, GREEN, PINK, ORANGE, YELLOW
+			RED,
+			BLUE,
+			GREEN,
+			PINK,
+			ORANGE,
+			YELLOW
 		};
 
 		public Color getColor() {
@@ -27,7 +39,8 @@ public class A {
 	@EntryPoint
 	void m() {
 		Collection<Widget> orderedWidgets = new ArrayList<>();
-		Map<Color, List<Widget>> widgetsByColor = orderedWidgets.stream()
-				.collect(Collectors.groupingByConcurrent(Widget::getColor));
+
+		Map<Color, Set<Widget>> widgetsByColor2 = orderedWidgets.stream().collect(Collectors.groupingByConcurrent(
+				Widget::getColor, ConcurrentSkipListMap::new, Collectors.toCollection(LinkedHashSet::new)));
 	}
 }
