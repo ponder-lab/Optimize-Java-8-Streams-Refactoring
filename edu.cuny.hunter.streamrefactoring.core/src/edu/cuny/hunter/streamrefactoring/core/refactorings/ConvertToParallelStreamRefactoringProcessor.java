62382a5be39a3bbb774d844cd96bba43f6306192
Raffi Khatchadourian <raffi.khatchadourian@hunter.cuny.edu>
2018-03-14 18:03:20 -0400

    Format.

--- edu.cuny.hunter.streamrefactoring.core/src/edu/cuny/hunter/streamrefactoring/core/refactorings/ConvertToParallelStreamRefactoringProcessor.java
+++ edu.cuny.hunter.streamrefactoring.core/src/edu/cuny/hunter/streamrefactoring/core/refactorings/ConvertToParallelStreamRefactoringProcessor.java
@@ -105,8 +105,7 @@ public class ConvertToParallelStreamRefactoringProcessor extends RefactoringProc
 	 * Minimum logging level. One of the constants in
 	 * org.eclipse.core.runtime.IStatus.
 	 *
-	 * @param level
-	 *            The minimum logging level to set.
+	 * @param level The minimum logging level to set.
 	 * @see org.eclipse.core.runtime.IStatus.
 	 */
 	public static void setLoggingLevel(int level) {
@@ -373,8 +372,7 @@ public class ConvertToParallelStreamRefactoringProcessor extends RefactoringProc
 	/**
 	 * Creates a working copy layer if necessary.
 	 *
-	 * @param monitor
-	 *            the progress monitor to use
+	 * @param monitor the progress monitor to use
 	 * @return a status describing the outcome of the operation
 	 */
 	private RefactoringStatus createWorkingCopyLayer(IProgressMonitor monitor) {
